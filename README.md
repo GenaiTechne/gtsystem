e9c86492e3a547a73d18061a4dedb3c117b994f4
GenAI Techne <159045165+GenaiTechne@users.noreply.github.com>
2024-04-19 19:33:14 -0700

    R 0.1.5 (Llama3 on Groq, Ollama)

--- README.md
+++ README.md
@@ -61,6 +61,11 @@ You can learn `gtsystem` API by following along the [notebook samples](https://g
 
 ## What's New
 
+### 2024-04-19 (Release 0.1.5)
+
+- Groq with Llama3
+- Ollama with Llama3
+
 ### 2024-04-19 (Release 0.1.4)
 
 - Stream chat and text for OpenAI
--- gtsystem/groq.py
+++ gtsystem/groq.py
@@ -7,8 +7,7 @@ GROQ = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-@metrics.track
-def text(prompt, system='', temperature=0.0, topP=1, tokens=512, model="mixtral-8x7b-32768"):
+def _groq_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model=""):
     chat_completion = GROQ.chat.completions.create(
         messages=[
             {
@@ -27,3 +26,30 @@ def text(prompt, system='', temperature=0.0, topP=1, tokens=512, model="mixtral-
     )
 
     return chat_completion.choices[0].message.content
+
+@metrics.track
+def mixtral_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model="mixtral-8x7b-32768"):
+    return _groq_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model=model)
+
+@metrics.track
+def llama3_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model="llama3-70b-8192"):
+    return _groq_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model=model)
+
+@metrics.track
+def llama2_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model="llama2-70b-4096"):
+    return _groq_text(prompt, system='', temperature=0.0, topP=1, tokens=512, model=model)
+
+
+def text(prompt, system='', temperature=0.0, topP=1, tokens=512, model="llama3"):
+    match model:
+        case 'mistral':
+            return mixtral_text(prompt, system=system, temperature=temperature, topP=topP, 
+                              tokens=tokens)
+        case 'llama3':
+            return llama3_text(prompt, system=system, temperature=temperature, topP=topP, 
+                              tokens=tokens)
+        case 'llama2':
+            return llama2_text(prompt, system=system, temperature=temperature, topP=topP, 
+                              tokens=tokens)
+        case _:
+            return 'Please specify a valid model name'
